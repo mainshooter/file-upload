d12b4d8ab82538cad64447e583de4ea6e6f487b3
Peter 4A5A <498883@edu.rocmn.nl>
2017-09-21 09:29:49 +0200

    Changed some of the controller

--- controller/uploadController.php
+++ controller/uploadController.php
@@ -12,8 +12,12 @@
     public function uploadFile() {
       if (ISSET($_POST['upload'])) {
         $this->FileUploader->setAllowedFileTypes('*');
-        $this->FileUploader->setSaveLocation('view/upload/');
-        echo $this->FileUploader->uploadFile($_FILES['fileUpload']);
+        $base_dir = getcwd();
+        $this->FileUploader->setSaveLocation($base_dir . '/view/upload/');
+
+        $uploadResult = $this->FileUploader->uploadFile($_FILES['fileUpload']);
+
+        echo '<img src=' . $GLOBALS['config']['base_url'] . 'view/upload/' . $_FILES['fileUpload']['name'] . '>';
       }
     }
 
