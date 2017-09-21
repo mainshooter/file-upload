6d2399091920630fb517760364846b94c5c217f8
Peter 4A5A <498883@edu.rocmn.nl>
2017-09-21 09:29:41 +0200

    Wrote the basic file uploader class

--- model/FileUploader.class.php
+++ model/FileUploader.class.php
@@ -1,23 +1,49 @@
 <?php
+  require_once 'model/Security.class.php';
 
   class FileUploader {
+    private $Security;
 
     private $allowedFileTypes;
+    private $saveLocation;
 
-    public function uploadFile() {
-
+    public function __construct() {
+      $this->Security = new Security();
     }
 
-    public function checkIfDirectoryExists() {
-
+    public function uploadFile($file) {
+      if ($this->checkIfDirectoryExists()) {
+        $uploadResult = move_uploaded_file($file['tmp_name'], $this->saveLocation . $file['name']);
+        return($uploadResult);
+      }
     }
 
-    public function checkIfFileTypeIsAllowed() {
+    private function checkIfDirectoryExists() {
+      if (file_exists($this->saveLocation) === true) {
+        return(true);
+      }
 
+      else {
+        return(false);
+      }
     }
 
-    public function setAllowedFileTypes() {
+    public function setSaveLocation($location) {
+      $this->saveLocation = $this->Security->checkInput($location);
+    }
 
+    private function checkIfFileTypeIsAllowed() {
+      if ($this->allowedFileTypes === '*') {
+        return(true);
+      }
+
+      else {
+        return(false);
+      }
+    }
+
+    public function setAllowedFileTypes($allowedFileTypes) {
+      $this->allowedFileTypes = $allowedFileTypes;
     }
 
   }
