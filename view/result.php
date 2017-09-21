036bfb596e3ceab0332b49b4ead06b664bc04697
Peter 4A5A <498883@edu.rocmn.nl>
2017-09-21 09:13:46 +0200

    Added the form

--- view/result.php
+++ view/result.php
@@ -1,59 +1,6 @@
-<?php
-  echo "
-    <div class='col-6 col-m-12'>
-      <h2>Here you can add your result.</h2>
-
-      <p>We have a lot of default styling. So you don't have to do it at your own!</p>
-      <div>Here is something that we have</div>
-      <button type='button'>Default button</button>
-
-      <table>
-        <tr>
-          <th>Naam</th>
-          <th>Leeftijd</th>
-          <th>Woonplaats</th>
-        </tr>
-        <tr>
-          <td>Peter</td>
-          <td>19</td>
-          <td>Tiel</td>
-        </tr>
-        <tr>
-          <td>Sander</td>
-          <td>16</td>
-          <td>Tiel</td>
-        </tr>
-      </table>
-
-      <form method='post'>
-        <label>Here we have our label</label>
-        <input type='text' name='test' placeholder='Here you can type'>
-        <div></div>
-        <select>
-          <option>Hoi</option>
-          <option>Doei</option>
-        </select>
-
-        <label>Text field</label>
-        <textarea placeholder='Here we have our textarea'></textarea>
-
-        <div></div>
-
-        <div></div>
-        <input type='submit' value='Our default submit button'>
-
-
-      </form>
-
-      <a href=''>Our default link is here!</a>
-
-      <ul>
-        <li>This</li>
-        <li>Is</li>
-        <li>Our</li>
-        <li>UL</li>
-      </ul>
-
-    </div>
-    ";
-?>
+<form class="col-6" method="post" enctype="multipart/form-data" action="<?php echo $GLOBALS['config']['base_url'] ?>upload/uploadFile">
+  <label>Bestand</label>
+  <input type="file" name="fileUpload">
+  <br>
+  <input type="submit" name="upload" value="Upload!">
+</form>
