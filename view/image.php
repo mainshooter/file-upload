afb22881e4d5556e2c649170ba03a4d4991bec8a
Peter 4A5A <498883@edu.rocmn.nl>
2017-09-21 09:57:50 +0200

    Changed the file location

--- view/image.php
+++ view/image.php
@@ -1,1 +1,1 @@
-<img src="view/upload/<?php echo $_FILES['fileUpload']['name']; ?>" class="col-6">
+<img src="<?php echo $GLOBALS['config']['base_url'] ?>view/upload/<?php echo $_FILES['fileUpload']['name']; ?>" class="col-6">
